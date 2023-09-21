06d4de82911295ab045d0038ba97618d1087ad11
wdq <a14907@outlook.com>
2023-09-21 16:36:41 +0800

    fix

--- EHentaiHelper/content-scripts/contentchange.js
+++ EHentaiHelper/content-scripts/contentchange.js
@@ -6,37 +6,43 @@ if (window.location.host == 'www.newtonsoft.com') {
 }
 
 if (window.location.host == 'endjin.com') {
-    document.querySelectorAll('.js-table-wrap').forEach(m=>m.classList.add('notranslate'))
+    document.querySelectorAll('.js-table-wrap').forEach(m => m.classList.add('notranslate'))
 }
 
 if (window.location.host == 'devblogs.microsoft.com') {
-    document.querySelectorAll('.table-responsive').forEach(m=>m.classList.add('notranslate'))
+    document.querySelectorAll('.table-responsive').forEach(m => m.classList.add('notranslate'))
 }
 
 if (window.location.host == 'learn.microsoft.com') {
-    document.querySelectorAll('#left-container').forEach(m=>m.classList.add('notranslate'))
-    document.querySelectorAll('table').forEach(m=>m.classList.add('notranslate'))
+    document.querySelectorAll('#left-container').forEach(m => m.classList.add('notranslate'))
+    document.querySelectorAll('table').forEach(m => m.classList.add('notranslate'))
+}
+
+if (window.location.host == 'www.juxiaoshuo.net') {
+    document.querySelectorAll('.header_wap').forEach(m => m.style.display = 'none')
+    document.querySelectorAll('.Readbtn').forEach(m => m.style.display = 'none')
+    document.querySelectorAll('.footer_wap').forEach(m => m.style.display = 'none')
+    document.body.style.backgroundImage = ''
 }
 
 if (window.location.host == 'tieba.baidu.com') {
-    setTimeout(function(){
-        document.querySelectorAll('.userbar').forEach(m=>m.style.visibility='hidden')
-        document.querySelector("body > ul").hidden=true
-        document.querySelector("#spage-tbshare-container").hidden=true
-    },1000)
+    setTimeout(function () {
+        document.querySelectorAll('.userbar').forEach(m => m.style.visibility = 'hidden')
+        document.querySelector("body > ul").hidden = true
+        document.querySelector("#spage-tbshare-container").hidden = true
+    }, 1000)
 
-    document.querySelectorAll('.search-sec').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.top-sec').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.left-sec').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.r-top-sec').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.r-right-sec').forEach(m=>m.style.visibility='hidden')
+    document.querySelectorAll('.search-sec').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.top-sec').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.left-sec').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.r-top-sec').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.r-right-sec').forEach(m => m.style.visibility = 'hidden')
 
-    
-    document.querySelectorAll('#head').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.right_section').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.card_top_wrap').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.nav_wrap').forEach(m=>m.style.visibility='hidden')
-    document.querySelectorAll('.p_author_face').forEach(m=>m.style.visibility='hidden')
-    
+
+    document.querySelectorAll('#head').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.right_section').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.card_top_wrap').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.nav_wrap').forEach(m => m.style.visibility = 'hidden')
+    document.querySelectorAll('.p_author_face').forEach(m => m.style.visibility = 'hidden')
+
 }
- 
