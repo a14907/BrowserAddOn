aa5339533ff5fa5de27e8901f22e58fcf1efd6a1
wdq <a14907@outlook.com>
2024-04-12 17:09:33 +0800

    'update'

--- EHentaiHelper/service-worker.js
+++ EHentaiHelper/service-worker.js
@@ -8,4 +8,43 @@ chrome.runtime.onInstalled.addListener(() => {
         }
     )
 
+    // var config = {
+    //     mode: "fixed_servers",
+    //     rules: {
+    //         proxyForHttp: {
+    //           scheme: "socks5",
+    //           host: "127.0.0.1",
+    //           port: 9080
+    //         },
+    //         proxyForHttps: {
+    //           scheme: "socks5",
+    //           host: "127.0.0.1",
+    //           port: 9080
+    //         }, 
+    //         fallbackProxy: {
+    //           scheme: "socks5",
+    //           host: "proxy.qkagame.cn",
+    //           port: 8080
+    //         },
+    //       bypassList: ["foobar.com"]
+    //     }
+    //   };
+    //   chrome.proxy.settings.set(
+    //     {value: config, scope: 'regular'},
+    //     function() {}
+    //   );    
+    
+    // var config = {
+    //     mode: "pac_script",
+    //     pacScript: {
+    //         url: "http://localhost:81/auto.pac",
+    //         mandatory: true
+    //     }
+    //   };
+    //   chrome.proxy.settings.set(
+    //     {value: config, scope: 'regular'},
+    //     function() {}
+    //   );
+
+
 })
